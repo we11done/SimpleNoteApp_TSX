1376ff01be3ba4c1f83122181c86ca9e56b9af2a
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-02 11:44:57 -0400

    implemented redux saga actions

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,6 +1,7 @@
 import * as types from './types';
 import { Memo } from '../models';
 
+// FETCH MEMO LIST Actions
 export type FetchMemoListAction = {
   type: typeof types.FETCH_MEMO_LIST_REQUEST;
 };
@@ -10,28 +11,57 @@ export type FetchMemoListSuccessAction = {
   payload: Memo[];
 };
 
+// FETCH MEMO Actions
 export type FetchMemoAction = {
-  type: typeof types.FETCH_MEMO;
-  payload: Memo;
-};
-
-export type AddMemoAction = {
-  type: typeof types.ADD_MEMO;
-  payload: Memo;
-};
-
-export type FetchDeletedMemoListAction = {
-  type: typeof types.FETCH_DELETED_MEMO_LIST;
-  payload: Memo[];
-};
-
-export type DeleteMemoAction = {
-  type: typeof types.DELETE_MEMO;
+  type: typeof types.FETCH_MEMO_REQUEST;
   payload: number;
 };
 
+export type FetchMemoSuccessAction = {
+  type: typeof types.FETCH_MEMO_SUCCESS;
+  payload: Memo;
+};
+
+// ADD MEMO Actions
+export type AddMemoAction = {
+  type: typeof types.ADD_MEMO_REQUEST;
+  payload: Memo;
+};
+
+export type AddMemoSuccessAction = {
+  type: typeof types.ADD_MEMO_SUCESSS;
+  payload: Memo;
+};
+
+// FETCH DELETED MEMO Actions
+export type FetchDeletedMemoListAction = {
+  type: typeof types.FETCH_DELETED_MEMO_LIST_REQUEST;
+};
+
+export type FetchDeletedMemoListSuccessAction = {
+  type: typeof types.FETCH_DELETED_MEMO_LIST_SUCCESS;
+  payload: Memo[];
+};
+
+// DELETED MEMO Actions
+export type DeleteMemoAction = {
+  type: typeof types.DELETE_MEMO_REQUEST;
+  payload: number;
+};
+
+export type DeleteMemoSuccessAction = {
+  type: typeof types.DELETE_MEMO_SUCCESS;
+  payload: number;
+};
+
+// RESTORE MEMO Actions
 export type RestoreMemoAction = {
-  type: typeof types.RESTORE_MEMO;
+  type: typeof types.RESTORE_MEMO_REQUEST;
+  payload: number;
+};
+
+export type RestoreMemoSuccessAction = {
+  type: typeof types.RESTORE_MEMO_SUCCESS;
   payload: number;
 };
 
@@ -39,37 +69,34 @@ export const fetchMemoList = (memos: Memo[]): FetchMemoListAction => ({
   type: types.FETCH_MEMO_LIST_REQUEST,
 });
 
-export const fetchMemo = (memo: Memo): FetchMemoAction => ({
-  type: types.FETCH_MEMO,
-  payload: memo,
+export const fetchMemo = (id: number): FetchMemoAction => ({
+  type: types.FETCH_MEMO_REQUEST,
+  payload: id,
 });
 
 export const addMemo = (memo: Memo): AddMemoAction => ({
-  type: types.ADD_MEMO,
+  type: types.ADD_MEMO_REQUEST,
   payload: memo,
 });
 
-export const fetchDeletedMemoList = (
-  memos: Memo[]
-): FetchDeletedMemoListAction => ({
-  type: types.FETCH_DELETED_MEMO_LIST,
-  payload: memos,
+export const fetchDeletedMemoList = (): FetchDeletedMemoListAction => ({
+  type: types.FETCH_DELETED_MEMO_LIST_REQUEST,
 });
 
 export const deleteMemo = (id: number): DeleteMemoAction => ({
-  type: types.DELETE_MEMO,
+  type: types.DELETE_MEMO_REQUEST,
   payload: id,
 });
 
 export const restoreMemo = (id: number): RestoreMemoAction => ({
-  type: types.RESTORE_MEMO,
+  type: types.RESTORE_MEMO_REQUEST,
   payload: id,
 });
 
 export type MemoActionTypes =
   | FetchMemoListSuccessAction
-  | FetchMemoAction
-  | AddMemoAction
-  | FetchDeletedMemoListAction
-  | DeleteMemoAction
-  | RestoreMemoAction;
+  | FetchMemoSuccessAction
+  | AddMemoSuccessAction
+  | FetchDeletedMemoListSuccessAction
+  | DeleteMemoSuccessAction
+  | RestoreMemoSuccessAction;
