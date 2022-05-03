6ae13052b9029f3642ffb74a328e10f3267e13ef
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-03 09:49:45 -0400

    adjusted logic according to the redux-saga implementation

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -4,7 +4,6 @@ import { Dispatch, bindActionCreators } from 'redux';
 import Home from '../pages/home';
 import { Memo } from '../models';
 import { RootState } from '../reducers';
-import * as api from '../apis';
 import {
   FetchMemoListAction,
   FetchDeletedMemoListAction,
@@ -15,29 +14,32 @@ import {
 type Props = {
   memos: Memo[];
   deletedMemos: Memo[];
-  fetchMemoList(memos: Memo[]): FetchMemoListAction;
-  fetchDeletedMemoList(memos: Memo[]): FetchDeletedMemoListAction;
+  apiCalling: boolean;
+  fetchMemoList(): FetchMemoListAction;
+  fetchDeletedMemoList(): FetchDeletedMemoListAction;
 };
 
 const HomeContainer = ({
   memos,
   deletedMemos,
+  apiCalling,
   fetchMemoList,
   fetchDeletedMemoList,
 }: Props) => {
   useEffect(() => {
-    const memos = api.fetchMemoList();
-    const deletedMemos = api.fetchDeletedMemoList();
-    fetchMemoList(memos);
-    fetchDeletedMemoList(deletedMemos);
-  }, [fetchMemoList, fetchDeletedMemoList]);
+    fetchMemoList();
+    fetchDeletedMemoList();
+  }, [fetchDeletedMemoList, fetchMemoList]);
 
-  return <Home memos={memos} deletedMemos={deletedMemos} />;
+  return (
+    <Home memos={memos} deletedMemos={deletedMemos} apiCalling={apiCalling} />
+  );
 };
 
 const mapStateToProps = (state: RootState) => ({
   memos: state.memo.memos,
   deletedMemos: state.memo.deletedMemos,
+  apiCalling: state.app.apiCalling,
 });
 
 const mapDispatchToProps = (dispatch: Dispatch) =>
