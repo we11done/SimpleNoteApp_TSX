1ad7a60bf554fcd3b45e991348a96dc1ea40144f
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-03 09:52:54 -0400

    added dialog model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,6 +1,11 @@
-export interface Memo {
+export type Memo = {
   id?: number;
   content: string;
   createdAt?: number;
   deleted?: boolean;
-}
+};
+
+export type Dialog = {
+  type: 'alter' | 'confirm';
+  text: string;
+};
