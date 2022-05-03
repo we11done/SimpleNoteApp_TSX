bf2a103fa177e6ef8b2e0d17107933f278c491bf
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-03 09:48:07 -0400

    adjusted logic according to the redux-saga implementation

--- src/containers/AddMemo.tsx
+++ src/containers/AddMemo.tsx
@@ -1,32 +1,36 @@
 import React, { useState } from 'react';
-import * as api from '../apis';
 import { connect } from 'react-redux';
 import { Dispatch, bindActionCreators } from 'redux';
 import { Navigate } from 'react-router-dom';
 import { addMemo, AddMemoAction } from '../actions';
+import { RootState } from '../reducers';
 import { Memo } from '../models';
 import AddMemo from '../pages/memo/NewMemo';
 type Props = {
+  apiCalling: boolean;
   addMemo(memo: Memo): AddMemoAction;
 };
 
-const AddMemoContainer = ({ addMemo }: Props) => {
+const AddMemoContainer = ({ addMemo, apiCalling }: Props) => {
   const [saved, setSaved] = useState<boolean>(false);
 
   const handleClickSave = (input: string) => {
     const content = input.trim();
     if (!content) return;
-    const memo = api.addMemo({ content });
-    addMemo(memo);
+    addMemo({ content });
     setSaved(true);
   };
 
   if (saved) return <Navigate to='/memo' />;
 
-  return <AddMemo handleClickSave={handleClickSave} />;
+  return <AddMemo handleClickSave={handleClickSave} apiCalling={apiCalling} />;
 };
+
+const mapStateToProps = (state: RootState) => ({
+  apiCalling: state.app.apiCalling,
+});
 
 const mapDispatchToProps = (dispath: Dispatch) =>
   bindActionCreators({ addMemo }, dispath);
 
-export default connect(null, mapDispatchToProps)(AddMemoContainer);
+export default connect(mapStateToProps, mapDispatchToProps)(AddMemoContainer);
