0b16b5194c4630ad22bd54e086613e407ffad74a
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-03 09:53:26 -0400

    added apiCalling app state to props

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -5,25 +5,33 @@ import Layout from '../../components/Layout';
 import Sidebar, { SidebarTitle } from '../../components/Sidebar';
 import Main from '../../components/Main';
 import AddMemoBtn from '../../components/AddMenuBtn';
+import Skelton from '../../components/Skelton';
 import { List, ListItem } from '../../components/List';
 
 type HomePageProps = {
+  apiCalling: boolean;
   memos: Memo[];
   deletedMemos: Memo[];
 };
 
-const HomePage = ({ memos, deletedMemos }: HomePageProps) => {
+const HomePage = ({ memos, deletedMemos, apiCalling }: HomePageProps) => {
   return (
     <Layout>
       <Sidebar>
         <SidebarTitle>Folders</SidebarTitle>
         <List>
-          <ListItem first>
-            <Link to='/memo'>Memo ({memos.length})</Link>
-          </ListItem>
-          <ListItem>
-            <Link to='/trash'>Trash ({deletedMemos.length})</Link>
-          </ListItem>
+          {apiCalling ? (
+            <Skelton style={{ margin: '10px' }} />
+          ) : (
+            <>
+              <ListItem first>
+                <Link to='/memo'>Memo ({memos.length})</Link>
+              </ListItem>
+              <ListItem>
+                <Link to='/trash'>Trash ({deletedMemos.length})</Link>
+              </ListItem>
+            </>
+          )}
         </List>
       </Sidebar>
       <Main>
