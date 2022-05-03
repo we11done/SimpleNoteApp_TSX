0fd7936f1b0ea0526b7b95fa9070528e7627ac88
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-03 09:55:57 -0400

    added app state to the root state

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import memo, { MemoState } from './memo';
+import app, { AppState } from './app';
 
 export type RootState = {
   memo: MemoState;
+  app: AppState;
 };
 
-const rootReducer = combineReducers({ memo });
+const rootReducer = combineReducers({ memo, app });
 
 export default rootReducer;
