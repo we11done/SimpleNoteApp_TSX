93647194c48848261244d571b6ad6e36c68de010
Jaehyun Jun <wjswogus0007@gmail.com>
2022-05-04 15:27:14 -0400

    added logic to give random delay to apis

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,29 +1,47 @@
 import { Memo } from '../models';
 
+const delays = [500, 750, 1000];
+const randomDelay = () => {
+  const randomIndex = Math.floor((Math.random() * 10) % delays.length);
+  const randomDelay = delays[randomIndex];
+  return randomDelay;
+};
+
 let store: Memo[] = [
   { id: 2, content: 'Second Memo', createdAt: Date.now() },
   { id: 1, content: 'First Memo', createdAt: Date.now() - 1 },
 ];
 
 export const fetchMemoList = () =>
-  store
-    .filter(memo => !memo.deleted)
-    .sort((a, b) => b.createdAt! - a.createdAt!);
+  new Promise(resolve => {
+    const memoList = store
+      .filter(memo => !memo.deleted)
+      .sort((a, b) => b.createdAt! - a.createdAt!);
+    setTimeout(() => resolve(memoList), randomDelay());
+  });
 
 export const fetchDeletedMemoList = () =>
-  store
-    .filter(memo => !!memo.deleted)
-    .sort((a, b) => b.createdAt! - a.createdAt!);
+  new Promise(resolve => {
+    const deletedMemoList = store
+      .filter(memo => !!memo.deleted)
+      .sort((a, b) => b.createdAt! - a.createdAt!);
+    setTimeout(() => resolve(deletedMemoList), randomDelay());
+  });
 
-export const fetchMemo = (memoId: number) => store.find(m => m.id === memoId);
+export const fetchMemo = (memoId: number) =>
+  new Promise((resolve, reject) => {
+    const memo = store.find(m => m.id === memoId);
+    setTimeout(() => (memo ? resolve(memo) : reject(404)), randomDelay());
+  });
 
-export const addMemo = (memo: Memo) => {
-  const lastMemo = store.sort((a, b) => b.id! - a.id!)[0];
-  memo.id = lastMemo ? lastMemo.id! + 1 : 1;
-  memo.createdAt = Date.now();
-  store = [memo, ...store];
-  return memo;
-};
+export const addMemo = (memo: Memo) =>
+  new Promise(resolve => {
+    const lastMemo = store.sort((a, b) => b.id! - a.id!)[0];
+    memo.id = lastMemo ? lastMemo.id! + 1 : 1;
+    memo.createdAt = Date.now();
+    store = [memo, ...store];
+    setTimeout(() => resolve(memo), randomDelay());
+  });
 
 export const deleteMemo = (memoId: number) => {
   store = store.map(memo => ({
